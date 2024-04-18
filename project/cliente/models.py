212d3e7d28ef418e779a80fa34dfbe66a3050af7
RAArrieta <alfredoarrieta99@gmail.com>
2024-04-18 11:52:53 -0300

    models

--- project/cliente/models.py
+++ project/cliente/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Pais(models.Model):
+    #Charfield solo indica que es un tipo str
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    nacimiento = models.DateField(null=True)
+    pais_origen_id = models.ForeignKey(Pais, on_delete=models.SET_NULL, blank= True, null=True)
+    #on_delete, si se elimina el pais en el cliente figurara NULL
+    #blank, hace que el campo no sea obligatorio
+
+    def __str__(self):
+        return f"{self.apellido} {self.nombre}"
