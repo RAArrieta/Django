7ad08a4626e37421e29cb5a6e8381065170d77f6
RAArrieta <alfredoarrieta99@gmail.com>
2024-04-21 11:25:55 -0300

    app producto

--- project/producto/models.py
+++ project/producto/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Categoria(models.Model):
+    """Categoria de los productos"""
+    nombre = models.CharField(max_length=200, unique=True)
+    # unique asegura que no se repita
+    descripcion = models.CharField(max_length=200, null=True, blank=True)
+    # null y blank en conjunto permiten que sea opcional el campo
+
+    def __str__(self) -> str:
+        return self.nombre
+
+# class Producto(models.Model):
+#     nombre = models.CharField(max_length=200)
+#     precio = models.CharField()
+
